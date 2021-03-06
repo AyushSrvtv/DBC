b93f6547c995fbcba89fddb22c33191629ee7446
AyushSrvtv <ayushsrvtv@gmail.com>
2021-03-06 17:36:46 +0530

    Adding Display Components in Angular Part

--- dbCompareUI/src/app/app.module.ts
+++ dbCompareUI/src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//Angular Material
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatTabsModule} from '@angular/material/tabs';
+import { SettingsComponent } from './settings/settings.component';
+import { ComparePanelComponent } from './compare-panel/compare-panel.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingsComponent,
+    ComparePanelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ dbCompareUI/src/app/compare-panel/compare-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-compare-panel',
+  templateUrl: './compare-panel.component.html',
+  styleUrls: ['./compare-panel.component.scss']
+})
+export class ComparePanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
